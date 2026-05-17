6b47e282f925bf8b8a9e3d48dcbefc7c1afef84b
d1vij <verma.divij@gmail.com>
2026-05-18 01:08:09 +0530

    chore: clean up student info and refactor folder structure of package/data to have a separate data directory instead of saving it at package root

--- packages/data/src/index.ts
+++ packages/data/src/index.ts
@@ -3,18 +3,18 @@ import path from "node:path";
 import { fetchForSchool } from "@cbse-wrapped/scraper";
 
 export async function generateData() {
-    const DataDir = path.join(__dirname, "../");
+    const DataDir = path.join(__dirname, "..", "data");
     console.log(`DataDir: ${DataDir}`);
 
     const davIn = Bun.file(path.join(DataDir, "student-data", "dav.json"));
     const davOut = Bun.file(path.join(DataDir, "results", "dav.json"));
     await fetchForSchool(davIn, davOut);
 
-    const orchidIn = Bun.file(
-        path.join(DataDir, "student-data", "orchid.json"),
-    );
-    const orchidOut = Bun.file(path.join(DataDir, "results", "orchid.json"));
-    await fetchForSchool(orchidIn, orchidOut);
+    // const orchidIn = Bun.file(
+    //     path.join(DataDir, "student-data", "orchid.json"),
+    // );
+    // const orchidOut = Bun.file(path.join(DataDir, "results", "orchid.json"));
+    // await fetchForSchool(orchidIn, orchidOut);
 }
 
 if (import.meta.main) {
